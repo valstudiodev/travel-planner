a37c2a7706b07e4dadc352ea5564451cceb81bf6
ukr23can <tkachenko6591@gmail.com>
2026-08-23 11:02:21 -0300

    add page 404

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { RouterProvider } from "react-router";
-import router from "./app/routes";
-
+import router from "@/app/routes";
 
 function App() {
   return <RouterProvider router={router} />
+
 }
 
 export default App;
